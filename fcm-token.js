0b19fe4463cd8fbd3cd37024da04b59a79acf2b9
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-10-28 01:47:53 +0200

    Pregătire server pentru Render

--- fcm-token.js
+++ fcm-token.js
@@ -1,25 +1,54 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import admin from "firebase-admin";
+
+// if (!process.env.FCM_SERVICE_ACCOUNT_JSON) {
+//   throw new Error("Lipsește variabila FCM_SERVICE_ACCOUNT_JSON în .env");
+// }
+
+// // Parse și corect private_key
+// const serviceAccountRaw = JSON.parse(process.env.FCM_SERVICE_ACCOUNT_JSON);
+// const serviceAccount = {
+//   ...serviceAccountRaw,
+//   private_key: serviceAccountRaw.private_key.replace(/\\n/g, "\n"),
+// };
+
+// // Initialize Firebase Admin
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export const getAccessToken = async () => {
+//   const token = await admin.credential.cert(serviceAccount).getAccessToken();
+//   return token.access_token;
+// };
+
 import dotenv from "dotenv";
 dotenv.config();
 
 import admin from "firebase-admin";
 
-if (!process.env.FCM_SERVICE_ACCOUNT_JSON) {
-  throw new Error("Lipsește variabila FCM_SERVICE_ACCOUNT_JSON în .env");
+const FCM_SERVICE_ACCOUNT_JSON = process.env.FCM_SERVICE_ACCOUNT_JSON;
+
+if (!FCM_SERVICE_ACCOUNT_JSON) {
+  throw new Error("Lipsește variabila FCM_SERVICE_ACCOUNT_JSON");
 }
 
-// Parse și corect private_key
-const serviceAccountRaw = JSON.parse(process.env.FCM_SERVICE_ACCOUNT_JSON);
-const serviceAccount = {
-  ...serviceAccountRaw,
-  private_key: serviceAccountRaw.private_key.replace(/\\n/g, "\n"),
-};
+const serviceAccount = JSON.parse(FCM_SERVICE_ACCOUNT_JSON);
 
-// Initialize Firebase Admin
+// Dacă private_key are \\n, înlocuim cu newline real
+if (serviceAccount.private_key.includes("\\n")) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export const getAccessToken = async () => {
-  const token = await admin.credential.cert(serviceAccount).getAccessToken();
-  return token.access_token;
+  const accessToken = await admin.credential
+    .cert(serviceAccount)
+    .getAccessToken();
+  return accessToken.token;
 };
